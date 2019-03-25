96458b7f73897d488cef29ff1f79d40df4c6f7b3
James Kusmambang <kusmambang@gmail.com>
2019-03-25 14:11:18 +0700

    oauth server

---
+++ simple-oauth2-server/src/main/java/com/paralun/app/SimpleOauth2ServerApplication.java
@@ -0,0 +1,13 @@
+package com.paralun.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleOauth2ServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SimpleOauth2ServerApplication.class, args);
+    }
+
+}
