5d71686271c0cf511dc76a940131ecebfaedb1fa
James Kusmambang <kusmambang@gmail.com>
2019-04-02 20:00:11 +0700

    initial

---
+++ belajar-oauth2-client/src/main/java/com/paralun/app/BelajarOauth2ClientApplication.java
@@ -0,0 +1,13 @@
+package com.paralun.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BelajarOauth2ClientApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BelajarOauth2ClientApplication.class, args);
+    }
+
+}
